0e47e08fe6e7f4ebf8a5cdca06b9d9d75e6c21a1
Ettore Passaro <6078569+ximera91@users.noreply.github.com>
2026-04-22 13:23:13 +0200

    fix: fixed tests attempting to write files on CI

--- ios/AuraTests/TestHelpers.swift
+++ ios/AuraTests/TestHelpers.swift
@@ -3,16 +3,5 @@ import SwiftData
 @testable import Aura
 
 func makeContainer() throws -> ModelContainer {
-    let schema = Schema([
-        FeelingType.self, TriggerType.self, SymptomType.self,
-        Medicine.self, FoodItem.self, ActivityType.self,
-        TellingSignType.self, ReliefMethodType.self,
-        SleepEntry.self, FeelingEntry.self, MedicineLog.self,
-        ActivityEntry.self, MealEntry.self, TriggerEntry.self,
-        SymptomEntry.self, HeadacheEntry.self, HeadachePainLog.self,
-        HeadacheSymptomLog.self, HeadacheMedicineLog.self,
-        HeadacheReliefLog.self, TreatmentSchedule.self,
-    ])
-    let config = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
-    return try ModelContainer(for: schema, configurations: [config])
+    try .makeAuraContainer(inMemory: true)
 }
